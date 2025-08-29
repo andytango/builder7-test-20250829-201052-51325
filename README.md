a9136ecdfba5c2295fe3a7ce6e2217421933da11
Andrew Hall <andrew@rioja.io>
2025-08-29 20:10:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# builder7-test-20250829-201052-51325
+
+Test repository for Builder-7 E2E testing.
+Created at: Fri Aug 29 20:10:54 BST 2025
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    """Main entry point of the application."""
+    print("Initial code")
+
+if __name__ == "__main__":
+    main()
